f3b0631b987185b7d4485e51429b95d5cc20434f
Nahush Dixit <nahush.dixit.mail@gmail.com>
2015-07-22 01:51:35 -0700

    Added remaining base configuration files

---
+++ emacs/default.el
@@ -0,0 +1,7 @@
+(defconst emacs-tmp-dir (format "%s%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
